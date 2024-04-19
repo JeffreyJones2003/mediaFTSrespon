6603073de12059525fd870f5961239ad116fb321
Jeffrey Jones S <135623574+JeffreyJones2003@users.noreply.github.com>
2024-04-19 20:54:06 +0530

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,29 @@ function linkAction(){
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// Photo Gallery Slider
+
+const modal=document.createElement('div');
+modal.id='modal';
+document.body.appendChild(modal);
+
+const images=document.querySelectorAll('.img');
+
+
+images.forEach(image=>{
+  image.addEventListener('click',()=>{
+    modal.classList.add('active');
+    const img=document.createElement('img');
+    img.src=image.src;
+    img.id="img";
+    while(modal.firstChild){
+      modal.removeChild(modal.firstChild);
+    }
+    modal.append(img);
+  });
+});
+
+modal.addEventListener('click',()=>{
+  modal.classList.remove('active');
+});
